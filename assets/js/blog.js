ed4e4272feb77a41e23d210235ba0a14d9219345
Alec Taber <alectaber12@gmail.com>
2024-09-09 12:04:10 -0700

    add createElement function to blog.js

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,6 +1,19 @@
 // TODO: Create a variable that selects the main element
+const mainElement = document.querySelector("main");
+const postsList = document.getElementById("posts");
 
 // TODO: Create a function that builds an element and appends it to the DOM
+function createElement(tag, attributes ={}, parent, content ='') {
+    const element = document.createElement(tag);
+    for (let key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    } if (content) {
+        element.textContent = content;
+    } if (parent) {
+        parent.appendChild(element);
+    }
+    return element;
+}
 
 // TODO: Create a function that handles the case where there are no blog posts to display
 
