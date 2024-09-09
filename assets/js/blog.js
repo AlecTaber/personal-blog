cb44c1ca3f19c69c40ee8b6e72b24a83e270b8be
Alec Taber <alectaber12@gmail.com>
2024-09-09 12:05:33 -0700

    add handleNoPosts and getBlogPosts functions to blog.js

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -16,7 +16,14 @@ function createElement(tag, attributes ={}, parent, content ='') {
 }
 
 // TODO: Create a function that handles the case where there are no blog posts to display
+function handleNoPosts() {
+    const noPostsMessage = createElement('p', { class: 'no-posts' }, postsList, 'No blog posts available.');
+  }
 
 // TODO: Create a function that reads from local storage and returns the data
+function getBlogPosts() {
+    const posts = JSON.parse(localStorage.getItem('blogPosts')) || [];
+    return posts;
+  }
 
 // TODO: Call the function to render the list of blog posts
